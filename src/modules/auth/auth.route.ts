d2dd1f4fcdd75319b1b5eb460217ab6a6008441e
aarohanbasnet <letsmailaarohan@gmail.com>
2026-07-31 16:20:09 +0545

    fix(auth) : remove zod validation for logout

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -11,7 +11,7 @@ const router = Router();
 
 router.post("/register", validate(registerUserSchema), registerController);
 router.post("/login", validate(loginUserSchema), loginController);
-router.post("/logout", validate(logoutSchema), logoutController);
+router.post("/logout", /*validate(logoutSchema),*/ logoutController);
 router.post("/verify-otp",validate(verifyOtpSchema), verifyOtpController);
 router.post("/refresh", validate(refreshTokenSchema), refreshTokenController);
 
