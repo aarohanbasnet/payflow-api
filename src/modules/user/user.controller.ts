27ad8746103ade54181d7a96aadfcba001449ba5
aarohanbasnet <letsmailaarohan@gmail.com>
2026-08-02 22:10:48 +0545

    feat(user) : add mpin setup endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getUserProfile } from "./user.service.js";
+import { getUserProfile, setUserMpin } from "./user.service.js";
 import { customRequest } from "../../middlewares/auth.middleware.js";
 import { AppError } from "../../utils/error.js";
 
@@ -20,3 +20,22 @@ export const userProfileController = async (
         data : profile
     });
 }
+
+export const setUserMpinController = async (
+    req : customRequest,
+    res : Response
+) : Promise<void> => {
+
+    const userId = req.user?.userId;
+    const mpin = req.body;
+
+    if(!userId){
+        throw new AppError("Unauthorized", 401);
+    };
+
+    await setUserMpin({userId, mpin});
+    res.status(200).json({
+        success : true,
+        message : "MPIN set successfully",
+    });
+};
