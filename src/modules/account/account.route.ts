ba1644c42bc30b18d6e31b8d8360f28a7c39a493
aarohanbasnet <letsmailaarohan@gmail.com>
2026-08-03 22:45:20 +0545

    feat(account):add account deposit, withdraw and retrievalroutes

---
+++ src/modules/account/account.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { depositController, withdrawController, getAccountController } from "./account.controller.js";
+import { isLoggedin } from "../../middlewares/auth.middleware.js";
+import { validate } from "../../middlewares/validate.middleware.js";
+import { depositInputSchema, withdrawInputSchema } from "./account.schema.js";
+
+const router = Router();
+
+router.post("/deposit", isLoggedin, validate(depositInputSchema), depositController);
+router.post("/withdraw", isLoggedin, validate(withdrawInputSchema), withdrawController);
+router.get("/", isLoggedin,  getAccountController);
+
+export default router;
