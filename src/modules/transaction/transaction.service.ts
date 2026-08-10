5af5e19a7d65a34f3b26731653686bf120459d3d
aarohanbasnet <letsmailaarohan@gmail.com>
2026-08-10 22:51:51 +0545

    feat(transaction): add utility payment service

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,13 +1,27 @@
+import { tr } from "zod/locales";
 import{ prisma } from "../../config/prisma.js";
-import { Prisma } from "../../generated/prisma/client.js";
+import { Prisma, UtilityType } from "../../generated/prisma/client.js";
 import { AppError } from "../../utils/error.js";
 import { generateReferenceNumber } from "../../utils/referenceNumber.js";
 import { checkDailyTransactionLimit } from "../services/limit.service.js";
 import { verifyUserMpin } from "../services/verify-mpin.service.js";
-import { TransferInput } from "./transaction.schema.js";
+import { TransferInput, UtilityInput } from "./transaction.schema.js";
+import { maskPhone } from "../../utils/mask.js";
 
 type TransferServiceInput = TransferInput & {
     userId : string
+}
+
+type UtilityServiceInput = UtilityInput & {
+    userId : string
+}
+
+const utilityVendors = {
+    ELECTRICITY :  ["NEA"],
+    WATER : ["KUKL"],
+    INTERNET : ["WORLDLINK", "VIANET", "SUBISU"],
+    MOBILE_TOPUP : ["NTC", "NCELL"],
+    TV : ["DISHHOME"]
 }
 
 export const transferAmount = async( input : TransferServiceInput)=> {
@@ -184,3 +198,93 @@ export const transferAmount = async( input : TransferServiceInput)=> {
             },
         };
 };
+
+export const utilityPayment = async ( input : UtilityServiceInput )=> {
+    const {utilityType, vendor, amount, mpin, userId} = input;
+
+    await verifyUserMpin(userId, mpin);
+
+    const initiator = await prisma.user.findUnique({
+        where : { id : userId },
+        select : {
+            name : true,
+            phone : true,
+            account : {
+                select : {
+                    id : true,
+                    accountNumber : true,
+                },
+            },  
+        },
+    });
+
+    if(!initiator || !initiator.account){
+        throw new AppError("Account not found", 404);
+    }
+
+    const initiatorAccountId = initiator.account.id;
+
+    const vendors = utilityVendors[utilityType];
+    if(!vendors.includes(vendor)){
+        throw new AppError(`${vendor} is not available for ${utilityType}`, 400)
+    }
+
+    await checkDailyTransactionLimit(initiatorAccountId, amount);
+    const referenceNumber = generateReferenceNumber();
+
+    const transactionRecord = await prisma.$transaction(
+
+        async (tx) => {
+            const initiatorAccount = await tx.account.findUnique({
+                where : { id : initiatorAccountId },
+                select : { balance : true },
+            });
+
+            const currentBalance = initiatorAccount?.balance.toNumber() ?? 0;
+            
+            if( currentBalance < amount ){
+                 throw new AppError("Insufficient acount balance", 400);
+            }
+
+            await tx.account.update({
+                where : { id : initiatorAccountId},
+                data : { 
+                    balance : { decrement : amount},
+                },
+                select : { balance : true }
+            });
+
+            return await tx.transaction.create({
+                data : {
+                    reference : referenceNumber,
+                    type : "UTILITY_PAYMENT",
+                    utilityType : utilityType,
+                    senderAccount : {
+                        connect : { id : initiatorAccountId } },
+                    amount : new Prisma.Decimal(amount),
+                    remarks : `${utilityType} bill payment to ${vendor}`,
+                    status : "SUCCESS"
+                }
+            });
+        });
+
+        return {
+            data : {
+                transactionId : transactionRecord.id,
+                referenceNumber : transactionRecord.reference,
+                initiator : {
+                    name : initiator.name,
+                    phone : maskPhone(initiator.phone),
+                },
+                utility : {
+                    type : transactionRecord.utilityType,
+                    vendor : vendor,
+                },
+                amount : transactionRecord.amount,
+                remarks : transactionRecord.remarks,
+                status : transactionRecord.status,
+                createdAt : transactionRecord.createdAt,
+            }
+        }
+
+}
