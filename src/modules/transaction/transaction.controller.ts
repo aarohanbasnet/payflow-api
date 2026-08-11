e11450871d3d00984b329eea989f7f0c75e20bfa
aarohanbasnet <letsmailaarohan@gmail.com>
2026-08-11 22:01:33 +0545

    feat(transaction) : add utility-payment controller

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -1,8 +1,9 @@
 import { Response } from "express";
 import { customRequest } from "../../middlewares/auth.middleware.js";
-import { TransferInput } from "./transaction.schema.js";
-import { transferAmount } from "./transaction.service.js";
+import { TransferInput, UtilityInput } from "./transaction.schema.js";
+import { transferAmount, utilityPayment } from "./transaction.service.js";
 import { AppError } from "../../utils/error.js";
+import { success } from "zod";
 
 export const transferAmountController = async(
     req : customRequest,
@@ -21,4 +22,22 @@ export const transferAmountController = async(
             data  : result.data,
         });
 
+    };
+
+export const utilityPaymentController = async(
+    req : customRequest,
+    res : Response )  => {
+        const input = req.body as UtilityInput;
+        const userId = req.user?.userId;
+
+        if(!userId){
+            throw new AppError("Unauthorized", 401);
+        }
+
+        const result = await utilityPayment( {userId, ...input});
+        res.status(201).json({
+            success : true,
+            message : "Payment successful",
+            data : result.data,
+        });
     };
