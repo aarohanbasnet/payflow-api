055815c1abb4f5fcf536e09227c6b908636304dd
aarohanbasnet <letsmailaarohan@gmail.com>
2026-08-11 22:02:46 +0545

    feat(transaction) : add utility-payment route

--- src/modules/transaction/transaction.route.ts
+++ src/modules/transaction/transaction.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { isLoggedin } from "../../middlewares/auth.middleware.js";
 import { validate } from "../../middlewares/validate.middleware.js";
-import { transferInputSchema } from "./transaction.schema.js";
-import { transferAmountController } from "./transaction.controller.js";
+import { transferInputSchema, utilityInputSchema } from "./transaction.schema.js";
+import { transferAmountController, utilityPaymentController } from "./transaction.controller.js";
 const router = Router();
 
 router.post("/transfer", isLoggedin, validate(transferInputSchema), transferAmountController);
+router.post("/utility-payment", isLoggedin, validate(utilityInputSchema), utilityPaymentController);
 
 export default router;
