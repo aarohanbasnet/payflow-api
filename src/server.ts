6e2e9c82ad847806fcac100ab435dba7427ef2c7
aarohanbasnet <letsmailaarohan@gmail.com>
2026-08-23 20:44:46 +0545

    feat(invoice) : add invoice route

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import cookieParser from "cookie-parser";
 import authRoutes from "../src/modules/auth/auth.route.js";
 import userRoutes from "../src/modules/user/user.routes.js"
 import { errorHandler } from "./middlewares/errorHandler.js";
-import accountRoutes from "../src/modules/account/account.route.js"
-import transactionRoutes from "../src/modules/transaction/transaction.route.js"
+import accountRoutes from "../src/modules/account/account.route.js";
+import transactionRoutes from "../src/modules/transaction/transaction.route.js";
+import invoiceRoutes from "../src/modules/invoice/invoice.route.js";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -19,6 +21,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/account", accountRoutes);
 app.use("/api/v1/transactions", transactionRoutes);
+app.use("/api/v1/transactions", invoiceRoutes);
 
 app.use(errorHandler);
 
