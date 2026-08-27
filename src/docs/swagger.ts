92085b41f8766a2ae42549319afacf57fc4d2a88
aarohanbasnet <letsmailaarohan@gmail.com>
2026-08-27 22:48:55 +0545

    chore(docs) : fix bugs

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import yaml from "js-yaml";
+import { load } from "js-yaml";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,4 +9,4 @@ const __dirname = path.dirname(__filename);
 const openapiPath = path.join(__dirname, "openapi.yaml");
 const file = fs.readFileSync(openapiPath, "utf8");
 
-export const swaggerSpec = yaml.load(file) as Record<string, unknown>;
+export const swaggerSpec = load(file) as Record<string, unknown>;
